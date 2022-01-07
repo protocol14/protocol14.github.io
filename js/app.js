d2fa5b8f0a96606159b416ebdba0709ab7b2324c
protocol14 <90670853+protocol14@users.noreply.github.com>
2022-01-08 00:34:53 +0900

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,91 @@
+'use strict';
+
+/*
+ * A Design by GraphBerry
+ * Author: GraphBerry
+ * Author URL: http://graphberry.com
+ * License: http://graphberry.com/pages/license
+ */
+/*
+ * 구글맵보다 카카오맵
+ */
+
+ // Open offsite navigation.
+ $('#nav-expander').on('click', function(e) {
+    e.preventDefault();
+    $('nav').toggleClass('nav-expanded');
+});
+
+// Close offsite navigation.
+ $('.menu .close').on('click', function(e) {
+    e.preventDefault();
+    $('nav').toggleClass('nav-expanded');
+});
+
+// Close offsite navigation after user click on an link in navigation.
+$('.menu  a').on('click', function(e) {
+    e.preventDefault();
+    $('nav').removeClass('nav-expanded');
+});
+
+
+//Calculate full with of jumbotron.
+ function homeFullScreen() {
+
+    var homeSection = $('.home');
+    var windowHeight = $(window).outerHeight();
+
+    if (homeSection.hasClass('home-fullscreen')) {
+
+        $('.home-fullscreen').css('height', windowHeight);
+    }
+}
+
+function openProject() {
+
+		    var portfolioItem = $('.portfolio-item  a');
+		    var singleProject = $('#single-project');
+
+		    portfolioItem.click(function () {
+
+			   var link = $(this).attr('href');
+
+
+			   singleProject.empty();
+
+       			   singleProject.slideDown(500)
+			       
+		       singleProject.load(link, function (response, status) {
+		       if (status === "error") {
+			      alert("An error");
+			} else {
+				 var closeProject = $('#close-project');
+				 closeProject.on('click', function () {
+				     singleProject.slideUp(500);
+				     setTimeout(function () {
+
+					 singleProject.empty();
+				     }, 500);
+				 });
+			      }
+			  });
+			   
+
+			  return false;
+		   });
+}	
+//Initialization 
+$(window).load(function () {
+    openProject();
+    homeFullScreen();
+
+    smoothScroll.init();
+});
+
+
+//What happen on window resize
+$(window).resize(function () {
+    homeFullScreen();
+});
+
+
