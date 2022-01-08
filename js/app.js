66654d112dee6b55b251b6c52c975b14da059b3b
protocol14 <90670853+protocol14@users.noreply.github.com>
2022-01-09 03:26:17 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -177,6 +177,7 @@ $(window).load(function () {
 //What happen on window resize
 $(window).resize(function () {
     homeFullScreen();
+	openProject();
 	
 	$('#single-project-1').empty()
 	$('#single-project-3').empty()
